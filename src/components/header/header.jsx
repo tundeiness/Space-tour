d49bac868242081594216aa53647ac79ac828410
tundeiness <tundeiness@yahoo.co.uk>
2021-12-02 22:36:36 +0100

    W.I.P header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <header className="header border-red">
       {/* <div className="pry-header__img-section border-red"> */}
-      <picture className="logo-wrap border-red">
+      <picture className="header__logo-wrap">
         <source srcSet={Logo} media="prefers-color-scheme: dark" />
         <img
           src={Logo}
@@ -25,33 +25,33 @@ const Header = () => {
       >
         <span className="sr-mobile" aria-expanded="false" />
       </button>
-      <nav className="navigation border-red">
-        <ul>
-          <li>
-            <a className="ff-sans" href="index.html">
-              <span>00</span>
-              Home
-            </a>
-          </li>
-          <li>
-            <a className="ff-sans" href="index.html">
-              <span>01</span>
-              Destination
-            </a>
-          </li>
-          <li>
-            <a className="ff-sans" href="index.html">
-              <span>02</span>
-              Crew
-            </a>
-          </li>
-          <li>
-            <a className="ff-sans" href="index.html">
-              <span>03</span>
-              Technology
-            </a>
-          </li>
-        </ul>
+      <nav className="nav">
+        {/* <ul> */}
+        <li>
+          <a className="ff-sans" href="index.html">
+            <span>00</span>
+            Home
+          </a>
+        </li>
+        <li>
+          <a className="ff-sans" href="index.html">
+            <span>01</span>
+            Destination
+          </a>
+        </li>
+        <li>
+          <a className="ff-sans" href="index.html">
+            <span>02</span>
+            Crew
+          </a>
+        </li>
+        <li>
+          <a className="ff-sans" href="index.html">
+            <span>03</span>
+            Technology
+          </a>
+        </li>
+        {/* </ul> */}
       </nav>
     </header>
   );
